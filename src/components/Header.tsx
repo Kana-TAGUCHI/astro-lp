9a2d3deb982ec8b2ff66c5a30f8bce1245d1adee
Kana Taguchi <mukrabwy103@gmail.com>
2023-07-18 20:53:05 -0700

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { FiChevronRight, FiExternalLink } from 'react-icons/fi';
+import { Link as ScrollLink } from 'react-scroll';
+import logo from '../images/logo.svg';
+
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const hamburgerLine = `block h-1 w-8 my-1 bg-gray-600 transition ease transform duration-300 rounded-full`;
+
+  const navMenu = [
+    { title: '特徴', link: 'feature' },
+    { title: '人材獲得までの流れ', link: 'flow' },
+    { title: 'よくある質問', link: 'faq' },
+  ];
+
+  return (
+    <header className='fixed z-10 w-full top-0 bg-white'>
+      <div className='flex justify-between items-center max-w-container mx-auto w-full h-full p-4 border-b border-gray-200 lg:py-4 lg:px-10'>
+        <a href='/' className='w-20 h-5 lg:w-28'>
+          <img src={logo} alt='Refit' />
+        </a>
+        <button
+          className='space-y-1 z-50 lg:hidden'
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span
+            className={`${hamburgerLine} ${
+              isOpen ? 'rotate-45 translate-y-2 opacity-100' : 'opacity-100'
+            }`}
+          ></span>
+          <span
+            className={`${hamburgerLine} ${
+              isOpen ? 'opacity-0' : 'opacity-100'
+            }`}
+          ></span>
+          <span
+            className={`${hamburgerLine} ${
+              isOpen ? '-rotate-45 -translate-y-2 opacity-100' : 'opacity-100'
+            }`}
+          ></span>
+        </button>
+
+        <nav
+          className={`flex flex-col absolute top-[57px] transition-all duration-500 h-screen w-full bg-white px-7 py-8 lg:static lg:h-full lg:flex-row lg:justify-end lg:items-center lg:bg-transparent lg:p-0 lg:gap-24 ${
+            isOpen ? 'right-0' : '-right-full'
+          }`}
+        >
+          <ul className='flex flex-col text-lg font-semibold items-center lg:flex-row lg:justify-end lg:text-md lg:leading-none lg:gap-8'>
+            {navMenu.map((item) => (
+              <li
+                key={item.link}
+                className='py-4 px-2 border-b border-gray-200 w-full lg:w-auto lg:border-b-0 lg:p-0'
+              >
+                <ScrollLink
+                  className='cursor-pointer flex items-center gap-2'
+                  to={item.link}
+                  spy={true}
+                  smooth={true}
+                  offset={-60}
+                  duration={650}
+                  onClick={() => setIsOpen(false)}
+                >
+                  <FiChevronRight className='text-main-600 text-3xl font-bold lg:hidden' />
+                  {item.title}
+                </ScrollLink>
+              </li>
+            ))}
+          </ul>
+          <div className='mt-10 flex flex-col gap-8 items-center lg:flex-row-reverse lg:mt-0 lg:gap-4'>
+            {/* TODO リンクURL */}
+            <a className='btn-primary lg:text-md lg:w-36 lg:py-2' href='/'>
+              新規登録
+            </a>
+            {/* TODO リンクURL */}
+            <a className='btn-secondary lg:text-md lg:w-36 lg:py-2' href='/'>
+              お問い合わせ
+            </a>
+            {/* TODO リンクURL */}
+            <a
+              href='/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='flex items-center justify-center font-semibold text-lg gap-1 pt-2 lg:text-sm lg:pt-0'
+            >
+              求職者はこちら
+              <FiExternalLink className='text-2xl font-bold lg:text-md' />
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,68 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Noto Sans JP', 'sans-serif'],
+      },
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        brand: '#4FD1C5',
+        main: {
+          50: '#F1FAFA',
+          100: '#E3F5F4',
+          200: '#B9E7E3',
+          300: '#8FD9D1',
+          400: '#4B9D96',
+          500: '#37928A',
+          600: '#2C756E',
+          700: '#276661',
+          800: '#215853',
+          900: '#1C4945',
+        },
+        orange: {
+          50: '#FFF2E3',
+          100: '#FFEAD1',
+          200: '#FFD8A9',
+          300: '#FFBB6B',
+          400: '#FFAF51',
+          500: '#FF961B',
+          600: '#E68718',
+          700: '#CC7816',
+          800: '#995A10',
+          900: '#663C0B',
+        },
+        gray: {
+          50: '#F7FAFC',
+          100: '#EDF2F7',
+          200: '#E2E8F0',
+          300: '#CBD5E0',
+          400: '#A0AEC0',
+          500: '#718096',
+          600: '#4A5568',
+          700: '#2D3748',
+          800: '#1A202C',
+          900: '#171923',
+        },
+      },
+      fontSize: {
+        '2xs': '0.625rem', // 10px
+        xs: '0.75rem', // 12px
+        sm: '0.875rem', // 14px
+        md: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem', // 20px
+        '2xl': '1.5rem', // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': '3.75rem', // 60px
+        '7xl': '4.5rem', // 72px
+        '8xl': '6rem', // 96px
+      },
+    },
+  },
+  plugins: [],
+};
